e3f31f94478f08728015b3b323725277f4fd074e
heidless <rob.lockhart@yahoo.co.uk>
2023-09-17 16:56:27 +0100

    working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react' 
+import api from './api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [transaction, setTransaction] = useState([]);
+  const [formData, setFormData] = useState({
+    amount: '',
+    category: '',
+    description: '',
+    is_income: false,
+    date: ''
+  });
+
+  const fetchTransactions = async () => {
+    const response = await api.get('transactions/');
+    setTransaction(response.data)
+  }
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  const handleInputChange = (event) => {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    setFormData({
+      ...formData,
+      [event.target.name]: value,
+    });
+  };
+
+  const handleFormSubmt = async (event) => {
+    event.preventDefault();
+    await api.post('/transactions/', formData);
+    fetchTransactions();
+    setFormData({
+      amount: '',
+      category: '',
+      description: '',
+      is_income: false,
+      date: ''
+    })
+  }
+
+  return(
+    <div>
+      <nav className='navbar, navbar-dark, bg-primary'>
+        <div className='container-fluid'>
+          <a className='navbar-brand' href='https://mimeworks.com/'>
+            Finance App
+          </a>  
+        </div>
+      </nav>
+
     </div>
-  );
+  )
 }
 
 export default App;
